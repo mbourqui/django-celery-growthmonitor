ac5c2440352832b2e4b356ca240e33aee97028a2
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-07-06 19:11:36 +0200

    Trigger build

--- celery_growthmonitor/__init__.py
+++ celery_growthmonitor/__init__.py
@@ -20,6 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __license__ = 'GNU GPLv3'
 __author__ = 'Marc Bourqui <https://github.com/mbourqui>'
-__version__ = '0.0.0-dev-0'
+__version__ = '0.0.0-dev-1'
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
 __status__ = 'Beta'
