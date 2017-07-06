fa14e03f9af0096751bcccaa5529e06b08332279
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-07-06 19:14:29 +0200

    Fix import

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 import os
 
-from echoices import __version__
+from celery_growthmonitor import __version__
 from setuptools import find_packages, setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
