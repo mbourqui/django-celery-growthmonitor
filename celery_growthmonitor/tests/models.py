36b769d2068461e709b95ed3220a99d45597706b
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-07-13 17:29:43 +0200

    Update tests

--- celery_growthmonitor/tests/models.py
+++ celery_growthmonitor/tests/models.py
@@ -1,5 +1,66 @@
-from celery_growthmonitor.models import AJob
+from django.db import models
+
+from celery_growthmonitor.models import AJob, ADataFile
 
 
 class TestJob(AJob):
     pass
+
+
+class MyRootStrTestJob(AJob):
+    job_root = 'my_root_str'
+
+
+def my_job_root(instance):
+    import os
+    return os.path.join('my_root_func', str(instance.pk))
+
+
+class MyRootFuncTestJob(AJob):
+    job_root = my_job_root
+
+
+class MyResultsStrTestJob(AJob):
+    upload_to_results = 'my_results_str'
+
+
+def my_job_results(instance, filename):
+    import os
+    return os.path.join('my_results_func', str(instance.pk), filename)
+
+
+class MyResultsFuncTestJob(AJob):
+    upload_to_results = my_job_results
+
+
+class MyRootResultsFuncTestJob(AJob):
+    job_root = my_job_root
+    upload_to_results = my_job_results
+
+
+class TestFile(ADataFile):
+    job = models.ForeignKey(TestJob, on_delete=models.CASCADE)
+
+
+class MyDataStrTestFile(ADataFile):
+    upload_to_data = 'my_data_str'
+    job = models.ForeignKey(TestJob, on_delete=models.CASCADE)
+
+
+def my_job_data(instance, filename):
+    import os
+    return os.path.join('my_data_func', filename)
+
+
+class MyDataFuncTestFile(ADataFile):
+    upload_to_data = my_job_data
+    job = models.ForeignKey(TestJob, on_delete=models.CASCADE)
+
+
+class MyRootFuncTestFile(ADataFile):
+    job = models.ForeignKey(MyRootFuncTestJob, on_delete=models.CASCADE)
+
+
+class MyRootDataFuncTestFile(ADataFile):
+    upload_to_data = my_job_data
+    job = models.ForeignKey(MyRootFuncTestJob, on_delete=models.CASCADE)
--- celery_growthmonitor/tests/tests.py
+++ celery_growthmonitor/tests/tests.py
@@ -1,18 +1,121 @@
 # -*- coding: utf-8 -*-
 
+import os
 import warnings
 
+from django.core.files.base import ContentFile
 from django.test import TestCase
 
-from celery_growthmonitor.models import MetaTask
-from celery_growthmonitor.tests.models import TestJob
-from celery_growthmonitor.tests.tasks import identity_task, constant_task, parametric_task
-from celery_growthmonitor.workflows import chain
+from celery_growthmonitor.tests import models
+from . import tasks
+from .. import settings
+from ..models import MetaTask
+from ..workflows import chain
 
 warnings.simplefilter("always")
 
 
-class Test(TestCase):
+class JobTestCase(TestCase):
+    def tearDown(self):
+        import os
+        import shutil
+        # Database is not kept but files would be otherwise
+        shutil.rmtree(os.path.join(settings.django_settings.MEDIA_ROOT, ))
+
+    def test_timezone(self):
+        from django.conf import settings
+        for switch in [False, True]:
+            settings.USE_TZ = switch
+            test_job = models.TestJob()
+            self.assertRaisesMessage(AssertionError, 'RuntimeWarning not triggered by save', self.assertWarnsRegex,
+                                     RuntimeWarning, 'DateTimeField TestJob.closure received a naive datetime',
+                                     test_job.save)
+
+    def test_job(self):
+        # Base case
+        expected_path = os.path.join(settings.APP_ROOT, 'testjob', '1', 'results')
+        self.assertFalse(os.path.isdir(expected_path))
+        test_job = models.TestJob()
+        test_job.save()
+        self.assertTrue(os.path.isdir(expected_path))
+        #
+        expected_path = os.path.join(settings.django_settings.MEDIA_ROOT, models.MyRootStrTestJob.job_root,
+                                     '1', 'results')
+        self.assertFalse(os.path.isdir(expected_path))
+        test_job = models.MyRootStrTestJob()
+        test_job.save()
+        self.assertTrue(os.path.isdir(expected_path))
+        #
+        expected_path = os.path.join(settings.django_settings.MEDIA_ROOT, 'my_root_func', '1', 'results')
+        self.assertFalse(os.path.isdir(expected_path))
+        test_job = models.MyRootFuncTestJob()
+        test_job.save()
+        self.assertTrue(os.path.isdir(expected_path))
+        #
+        expected_path = os.path.join(settings.APP_ROOT, 'myresultsstrtestjob', '1', 'my_results_str')
+        self.assertFalse(os.path.isdir(expected_path))
+        test_job = models.MyResultsStrTestJob()
+        test_job.save()
+        self.assertTrue(os.path.isdir(expected_path))
+        #
+        expected_path = os.path.join(settings.APP_ROOT, 'myresultsfunctestjob', '1', 'my_results_func')
+        self.assertFalse(os.path.isdir(expected_path))
+        test_job = models.MyResultsFuncTestJob()
+        test_job.save()
+        self.assertTrue(os.path.isdir(expected_path))
+        #
+        expected_path = os.path.join(settings.django_settings.MEDIA_ROOT, 'my_root_func', '1', 'my_results_func')
+        self.assertFalse(os.path.isdir(expected_path))
+        test_job = models.MyRootResultsFuncTestJob()
+        test_job.save()
+        self.assertTrue(os.path.isdir(expected_path))
+
+    def test_job_with_file(self):
+        test_file = ContentFile('DUMMY CONTENT', 'foobar.txt')
+        # Base case
+        test_job = models.TestJob()
+        test_job.save()
+        expected_path = os.path.join(settings.APP_ROOT, 'testjob', '1', 'data', 'foobar.txt')
+        self.assertFalse(os.path.exists(expected_path))
+        annotation = models.TestFile(job=test_job, data=test_file)
+        annotation.save()
+        self.assertTrue(os.path.exists(expected_path))
+        #
+        expected_path = os.path.join(settings.APP_ROOT, 'testjob', '1', 'my_data_str', 'foobar.txt')
+        self.assertFalse(os.path.exists(expected_path))
+        annotation = models.MyDataStrTestFile(job=test_job, data=test_file)
+        annotation.save()
+        self.assertTrue(os.path.exists(expected_path))
+        #
+        expected_path = os.path.join(settings.APP_ROOT, 'testjob', '1', 'my_data_func', 'foobar.txt')
+        self.assertFalse(os.path.exists(expected_path))
+        annotation = models.MyDataFuncTestFile(job=test_job, data=test_file)
+        annotation.save()
+        self.assertTrue(os.path.exists(expected_path))
+        #
+        #
+        test_job = models.MyRootFuncTestJob()
+        test_job.save()
+        expected_path = os.path.join(settings.django_settings.MEDIA_ROOT, 'my_root_func', '1', 'data', 'foobar.txt')
+        self.assertFalse(os.path.exists(expected_path))
+        annotation = models.MyRootFuncTestFile(job=test_job, data=test_file)
+        annotation.save()
+        self.assertTrue(os.path.exists(expected_path))
+        #
+        expected_path = os.path.join(settings.django_settings.MEDIA_ROOT, 'my_root_func', '1', 'my_data_func',
+                                     'foobar.txt')
+        self.assertFalse(os.path.exists(expected_path))
+        annotation = models.MyRootDataFuncTestFile(job=test_job, data=test_file)
+        annotation.save()
+        self.assertTrue(os.path.exists(expected_path))
+
+
+class TasksTestCase(TestCase):
+    def setUp(self):
+        self.job = models.TestJob()
+        self.job.save()
+        self.mt = MetaTask(self.job)
+
     def tearDown(self):
         from django.conf import settings
         import os
@@ -21,10 +124,7 @@ class Test(TestCase):
         shutil.rmtree(os.path.join(settings.CELERY_GROWTHMONITOR_APP_ROOT, ))
 
     def test_no_task(self):
-        test_job = TestJob()
-        test_job.save()
-        mt = MetaTask(test_job)
-        workflow = chain(mt, )
+        workflow = chain(self.mt, )
         result = workflow.apply_async(debug=True)
         self.assertEqual(result.state, 'SUCCESS')
         self.assertEqual(result.status, 'SUCCESS')
@@ -32,10 +132,7 @@ class Test(TestCase):
         self.assertIsInstance(result.result[1], tuple)
 
     def test_identity_task(self):
-        test_job = TestJob()
-        test_job.save()
-        mt = MetaTask(test_job)
-        workflow = chain(mt, identity_task.s())
+        workflow = chain(self.mt, tasks.identity_task.s())
         result = workflow.apply_async(debug=True)
         self.assertEqual(result.state, 'SUCCESS')
         self.assertEqual(result.status, 'SUCCESS')
@@ -43,10 +140,7 @@ class Test(TestCase):
         self.assertIsInstance(result.result.results, tuple)
 
     def test_constant_task(self):
-        test_job = TestJob()
-        test_job.save()
-        mt = MetaTask(test_job)
-        workflow = chain(mt, constant_task.s())
+        workflow = chain(self.mt, tasks.constant_task.s())
         result = workflow.apply_async(debug=True)
         self.assertEqual(result.state, 'SUCCESS')
         self.assertEqual(result.status, 'SUCCESS')
@@ -55,11 +149,8 @@ class Test(TestCase):
         self.assertIs(result.result[1][0], True)
 
     def test_parametric_task(self):
-        test_job = TestJob()
-        test_job.save()
-        mt = MetaTask(test_job)
         args = (True, 2)
-        workflow = chain(mt, parametric_task.s(*args))
+        workflow = chain(self.mt, tasks.parametric_task.s(*args))
         result = workflow.apply_async(debug=True)
         self.assertEqual(result.state, 'SUCCESS')
         self.assertEqual(result.status, 'SUCCESS')
@@ -67,12 +158,3 @@ class Test(TestCase):
         self.assertIsInstance(result.result.results, tuple)
         self.assertIs(result.result.results[0], True)
         self.assertEqual(result.result.results[1], 2)
-
-    def test_timezone(self):
-        from django.conf import settings
-        for switch in [False, True]:
-            settings.USE_TZ = switch
-            test_job = TestJob()
-            self.assertRaisesMessage(AssertionError, 'RuntimeWarning not triggered by save', self.assertWarnsRegex,
-                                     RuntimeWarning, 'DateTimeField TestJob.closure received a naive datetime',
-                                     test_job.save)
