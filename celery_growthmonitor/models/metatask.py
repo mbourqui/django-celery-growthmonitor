d7d3c78f11dbbe665661e080d7efc42651c5f1bf
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-08-15 18:36:23 +0200

    Replate dict with SimpleNamespace

--- celery_growthmonitor/models/metatask.py
+++ celery_growthmonitor/models/metatask.py
@@ -2,6 +2,21 @@ import logging
 from datetime import datetime
 
 from celery_growthmonitor.models.job import AJob
+try:
+    from types import SimpleNamespace
+except ImportError:
+    # SEE: https://docs.python.org/3/library/types.html#types.SimpleNamespace
+    class SimpleNamespace:
+        def __init__(self, **kwargs):
+            self.__dict__.update(kwargs)
+
+        def __repr__(self):
+            keys = sorted(self.__dict__)
+            items = ("{}={!r}".format(k, self.__dict__[k]) for k in keys)
+            return "{}({})".format(type(self).__name__, ", ".join(items))
+
+        def __eq__(self, other):
+            return self.__dict__ == other.__dict__
 
 logger = logging.getLogger(__name__)
 
@@ -64,7 +79,7 @@ class MetaTask:
         return self.duration
 
     def failed(self, task, exception):
-        self.error = dict(task=task.__name__, exception=exception)
+        self.error = SimpleNamespace(task=task.__name__, exception=exception)
         # TODO: http://stackoverflow.com/questions/4564559/
         logger.exception("Task %s failed with following exception: %s", task.__name__, exception)
 
