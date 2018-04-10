4420e54139a4d70d2c7cf48d69a9aadcd67824ff
Marc Bourqui <m.bourqui@edsi-tech.com>
2018-04-10 15:25:06 +0200

    Open root_job path generator

--- celery_growthmonitor/models/__init__.py
+++ celery_growthmonitor/models/__init__.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 
 from .job import AJob, ADataFile
-from .job import user_path, job_root, job_data, job_results
+from .job import user_path, root_job, job_root, job_data, job_results
 from .jobholder import JobHolder
