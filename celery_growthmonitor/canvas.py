3d55f4022cd1a59b4375f5bc4f2ee2e7e1527cdc
Marc Bourqui <m.bourqui@edsi-tech.com>
2018-06-29 18:02:41 +0200

    Support partial chains

--- celery_growthmonitor/canvas.py
+++ celery_growthmonitor/canvas.py
@@ -1,10 +1,43 @@
 from __future__ import absolute_import, unicode_literals
 
+import sys
+
 from celery.canvas import chain as celery_chain
 
 from celery_growthmonitor import settings
 from celery_growthmonitor.models import JobHolder
 from celery_growthmonitor.tasks import remove_old_jobs, start, stop
+
+
+def pre(job_holder: JobHolder, *tasks):
+    if sys.version_info < (3, 5):
+        flow = "(start.s(job_holder),"
+        if tasks:
+            flow += "*tasks,"
+    else:
+        flow = (start.s(job_holder),)
+        if tasks:
+            flow += tuple([task for task in tasks])
+    return flow
+
+
+def post(*tasks):
+    if sys.version_info < (3, 5):
+        flow = ""
+        if tasks:
+            flow += "*tasks,"
+        flow += "stop.s()"
+        if settings.TTL.seconds > 0:
+            flow += ",remove_old_jobs.s()"
+        flow += ")"
+    else:
+        flow = ()
+        if tasks:
+            flow += tuple([task for task in tasks])
+        flow += (stop.s(),)
+        if settings.TTL.seconds > 0:
+            flow += (remove_old_jobs.s(),)
+    return flow
 
 
 def chain(job_holder: JobHolder, *tasks):
@@ -22,17 +55,27 @@ def chain(job_holder: JobHolder, *tasks):
 
     """
     try:
-        flow = eval("""(
-            start.s(job_holder),
-            *tasks,
-            stop.s(),
-            remove_old_jobs.s(),
-        )""")
+        flow = eval(pre(job_holder, tasks) + post())
     except SyntaxError:
         # Python < 3.5
-        flow = (start.s(job_holder),)
-        flow += tuple([task for task in tasks])
-        flow += (stop.s(),)
-        if settings.TTL.seconds > 0:
-            flow += (remove_old_jobs.s(),),
+        flow = pre(job_holder, tasks)
+        flow += post()
+    return celery_chain(*flow)
+
+
+def chain_pre(job_holder: JobHolder, *tasks):
+    try:
+        flow = eval(pre(job_holder, tasks))
+    except SyntaxError:
+        # Python < 3.5
+        flow = pre(job_holder,  tasks)
+    return celery_chain(*flow)
+
+
+def chain_post(*tasks):
+    try:
+        flow = eval(post(tasks))
+    except SyntaxError:
+        # Python < 3.5
+        flow = post(tasks)
     return celery_chain(*flow)
