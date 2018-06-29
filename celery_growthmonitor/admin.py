a3c48b4cae3aa1ec818961bb370a4e6f34aa757b
Marc Bourqui <m.bourqui@edsi-tech.com>
2018-06-29 16:39:57 +0200

    Add admin mixin

--- celery_growthmonitor/admin.py
+++ celery_growthmonitor/admin.py
@@ -1,6 +1,9 @@
 from abc import ABCMeta
 
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.translation import ugettext_lazy as _
 
 
 class AJobAdmin(admin.ModelAdmin):
@@ -27,3 +30,26 @@ class AFieldsForDataFileInlineModelAdmin(admin.options.InlineModelAdmin):
 
     def has_add_permission(self, request):
         return False
+
+
+class HasJobAdminMixin:
+    app_label = None
+    job_model = None
+    job_label = _("Job")
+
+    list_display = ('render_job',)
+    list_filter = ()
+    search_fields = ('render_job',)
+    fields = ('job_link',)
+    readonly_fields = ('job_link',)
+
+    def render_job(self, obj):
+        return str(obj.job)
+
+    render_job.short_description = job_label
+
+    def job_link(self, obj):
+        url = reverse('admin:{}_{}_change'.format(self.app_label, self.job_model.__name__.lower()), args=(obj.job.pk,))
+        return format_html('<a href="%s">%s</a>' % (url, self.render_job(obj)))
+
+    job_link.short_description = job_label
